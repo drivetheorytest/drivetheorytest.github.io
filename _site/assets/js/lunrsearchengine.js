14abd97180fa79401e51e5075023400cb321dbf4
iamthenewking <mrgabonijeff@gmail.com>
2024-05-06 22:10:50 +0800

    add page test

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -37,7 +37,7 @@ var documents = [{
     }, {
     "id": 7,
     "url": "http://localhost:4000/test",
-    "title": "",
+    "title": "Mock Test",
     "body": "&lt;!DOCTYPE html&gt;          {% seo %}                       Navigation  Home  Mock Test {% include search-lunr. html %}                           {{ site. cta-head }}:   {{ site. cta-text}}     Get Started                     Copyright © {{ site. time | date:  %Y  }} {{ site. name }}          Follow                                   &lt;/footer&gt;{% if site. lazyimages ==  enabled  %}{% endif %}{% include cookie-consent. html %}&lt;/body&gt;&lt;/html&gt;"
     }, {
     "id": 8,
