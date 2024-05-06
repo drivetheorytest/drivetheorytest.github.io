20694d90c7bedff1e4392d5a730c1b911a30493d
iamthenewking <mrgabonijeff@gmail.com>
2024-05-07 06:15:25 +0800

    update index

--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -38,7 +38,7 @@ var documents = [{
     "id": 7,
     "url": "http://localhost:4000/test",
     "title": "Mock Test",
-    "body": "&lt;!DOCTYPE html&gt;          {% seo %}                       Navigation  Home  Mock Test {% include search-lunr. html %}                           {{ site. cta-head }}:   {{ site. cta-text}}     Get Started                     Copyright © {{ site. time | date:  %Y  }} {{ site. name }}          Follow                                   &lt;/footer&gt;{% if site. lazyimages ==  enabled  %}{% endif %}{% include cookie-consent. html %}&lt;/body&gt;&lt;/html&gt;"
+    "body": "&lt;!DOCTYPE html&gt;          {% seo %}                       Navigation  Home  Mock Test {% include search-lunr. html %}                    {{ site. cta-head }}:   {{ site. cta-text}}     Get Started                     Copyright © {{ site. time | date:  %Y  }} {{ site. name }}          Follow                                   &lt;/footer&gt;{% if site. lazyimages ==  enabled  %}{% endif %}{% include cookie-consent. html %}&lt;/body&gt;&lt;/html&gt;"
     }, {
     "id": 8,
     "url": "http://localhost:4000/boost-your-confidence-before-your-driving-theory-test/",
